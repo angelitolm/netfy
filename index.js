e0a6b6e0a4d2e9bebf605c663e1c16b71b77f264
Angel Labrada Massó <angel_labrada@edicuba.artex.cu>
2019-01-04 14:03:26 -0500

    Refactoring Code

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
+/*
+  This file is a part of Netfy
+  Author: Angel Labrada Massó
+ */
 'use strict'
 
+// ===============================================================
 // Import Modules
+// ===============================================================
 const app = require('./server')
 const mongoose = require('mongoose')
 
+// ===============================================================
 // Settings
+// ===============================================================
 const { dbUri } = require('./src/configs/database')
 const { port } = require('./src/configs/app')
 
+// ===============================================================
 // Connection to Database
+// ===============================================================
 mongoose.set('useCreateIndex', true)
 mongoose.connect(dbUri, { useNewUrlParser: true })
   .then(db => console.log('MongoDB is running'))
   .catch(err => console.error(err))
 
+// ===============================================================
 // Starting the server
+// ===============================================================
 app.listen(port, () => {
   console.log(`API Rest Netfy running on http://localhost:${port}`)
 })
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+/*
+  This file is a part of Netfy
+  Author: Angel Labrada Massó
+ */
 'use strict'
 
+// ===============================================================
 // Import Modules
+// ===============================================================
 const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
@@ -10,16 +16,17 @@ const session = require('express-session')
 const api = require('./src/routes/app.routes')
 const { SECRET_TOKEN } = require('./src/configs/passport')
 
-// Database Config
-// const { mongoose } = require('./index')
-
-// Initializing App
+// ===============================================================
+// Initializing App with Express Framework
+// ===============================================================
 const app = express()
 
+// ===============================================================
 // Middleware
-
+// ===============================================================
+// Debug by console everything the requests to making on web server
 app.use(morgan('dev'))
-
+// App use format json for all requests
 app.use(express.json())
 // Using Session Variables
 app.use(session({
@@ -34,7 +41,9 @@ app.use(passport.session())
 // Save Flash Messages
 app.use(flash())
 
+// ===============================================================
 // Routes API
+// ===============================================================
 app.use('/', api)
 
 // Route Login
@@ -42,7 +51,9 @@ app.get('/login', (req, res) => {
   res.send({ message: 'login' })
 })
 
+// ===============================================================
 // Static files
+// ===============================================================
 app.use(express.static(path.join(__dirname, 'src/public')))
 
 module.exports = app
