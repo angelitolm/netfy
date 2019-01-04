d44f21b3da06e16fb7b8b97951e99441a3c107f9
Angel Labrada Massó <angel_labrada@edicuba.artex.cu>
2019-01-04 11:50:26 -0500

    Refactoring Code

---
+++ src/configs/database.js
@@ -0,0 +1,6 @@
+'use strict'
+
+// Global Settings
+module.exports = {
+  dbUri: process.env.MONGODB || 'mongodb://localhost:27017/netfy'
+}
---
+++ src/configs/passport.js
@@ -0,0 +1,5 @@
+'use strict'
+
+module.exports = {
+  SECRET_TOKEN: 'Mypassword'
+}
