3e20e1643956d6556e90cce619a69d9af920efa7
Angel Labrada Massó <angelitolabm@gmail.com>
2019-01-04 19:04:44 -0500

    Route Dashboard
    Added this route

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -9,3 +9,14 @@
 // ===============================================================
 const express = require('express')
 const router = express.Router()
+const { isAuthenticated } = require('../middelwares/auth')
+
+// ===============================================================
+// Routes
+// ===============================================================
+// Dashboard
+router.get('/dashboard', isAuthenticated, async (req, res) => {
+  res.status(200).send({ message: 'You are in dashboard' })
+})
+
+module.exports = router
